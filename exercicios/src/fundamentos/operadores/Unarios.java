326bac1245ed2893c092815c2b71e44885a742ec
matheusap <bigmatheusmh@gmail.com>
2025-02-12 17:35:40 -0300

    -

--- exercicios/src/fundamentos/operadores/Unarios.java
+++ exercicios/src/fundamentos/operadores/Unarios.java
@@ -3,7 +3,9 @@ package fundamentos.operadores;
 public class Unarios {
 	public static void main(String[] args) {
 		
+		@SuppressWarnings("unused")
 		int a = 1;
+		@SuppressWarnings("unused")
 		int b = 2;
 		
 		a++; // a = a + 1
